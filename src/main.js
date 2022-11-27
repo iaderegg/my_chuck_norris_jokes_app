b44fae65ba21269f4ab6f99bcbc348e6450311b5
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-11-27 04:13:38 -0500

    Netlify settings

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "./assets/main.css";
 
-import VueSweetalert from 'vue-sweetalert2';
+import VueSweetalert from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
 
 const app = createApp(App);
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/docs/1.0/'  // This is whatever your path from the root is
+      : '/'
+  }
