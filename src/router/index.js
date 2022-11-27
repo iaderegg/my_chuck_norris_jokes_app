765053b1e21bc2fc4bce06bcfba643d4a5c33511
Iader E. Garcia Gomez <iadergg@gmail.com>
2022-11-27 04:17:54 -0500

    Netlify settings

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, hash } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: hash(),
   routes: [
     {
       path: "/",
